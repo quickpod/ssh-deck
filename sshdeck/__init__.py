eed9cf08e98570912a1b05bbfa3dc0488505015c
Anil Singh <anilsingh@dosvak.com>
2026-08-18 01:01:47 +0000

    1.2.1: require cryptography >= 44 so the vault uses Argon2id

--- sshdeck/__init__.py
+++ sshdeck/__init__.py
@@ -19,7 +19,7 @@ from . import sessions, keys, forward, client, sftp
 from .sessions import Session
 from .forward import ForwardSpec, parse_forward
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 
 __all__ = [
     "SSHDeckError",
